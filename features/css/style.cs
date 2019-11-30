b41de5030557f0176c3d47f44818347b418d1110
audreysaudjhana <56724687+audreysaudjhana@users.noreply.github.com>
2019-11-30 20:51:10 +0700

    First Upload

---
+++ features/css/style.cs
@@ -0,0 +1,19 @@
+.jumbotron {
+	background-image: url('image/sung1.jpg');
+	background-size: cover;
+	height: 600px;
+}
+
+.jumbotron .display-4 {
+	margin-top: 150px;
+	font-weight: bold;
+}
+
+.card {
+	width: 20rem;
+	margin-top: -150px;
+}
+
+td {
+	text-align: center;
+}
